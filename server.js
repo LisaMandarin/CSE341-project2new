f9617bc5b9985bd000bd46bca89b3dca1e9f426b
LisaMandarin <chu23001@byui.edu>
2024-11-29 02:00:25 +0800

    node project init

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+require("dotenv").config()
+const app = express()
+const port = process.env.PORT
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(port, () => {console.log(`Server application listening on port ${port}`)})
